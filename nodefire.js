1a261ce31077f63dc4f9de3031f092f9e350007b
jwngr <wenger.jacob@gmail.com>
2018-07-14 09:58:28 -0400

    Move firebase-childrenkeys directly into Nodefire

--- nodefire.js
+++ nodefire.js
@@ -4,13 +4,8 @@ const Firebase = require('firebase');
 const FirebaseTokenGenerator = require('firebase-token-generator');
 const _ = require('lodash');
 const url = require('url');
+const request = require('request');
 const LRUCache = require('lru-cache');
-
-try {
-  require('firebase-childrenkeys');
-} catch (e) {
-  // ignore, not available
-}
 
 let cache, cacheHits = 0, cacheMisses = 0, maxCacheSizeForDisconnectedHost = Infinity;
 const serverTimeOffsets = {}, serverDisconnects = {};
@@ -505,6 +500,39 @@ class NodeFire {
   }
 
   /**
+   * Tries to fetch they keys of all the children of this node, without also fetching all the
+   * contents, using the Firebase REST API.
+   * @param options An options object with the following items, all optional:
+   *   - auth: an auth token to pass to the REST API
+   *   - maxTries: the maximum number of times to try to fetch the keys, in case of transient errors
+   *               (defaults to 1)
+   *   - retryInterval: the number of milliseconds to delay between retries (defaults to 1000)
+   * @return A promise that resolves to an array of key strings.
+   */
+  childrenKeys(options = {}) {
+    const uri = this.toString() + '.json';
+    const qs = {shallow: true};
+    if (options.auth) qs.auth = options.auth;
+    return new Promise((resolve, reject) => {
+      let tries = 0;
+      function tryRequest() {
+        tries++;
+        request({uri, qs}, function(error, response, data) {
+          if (error && options.maxTries && tries < options.maxTries) {
+            setTimeout(tryRequest, options.retryInterval || 1000);
+          } else if (error) {
+            reject(error);
+          } else {
+            const object = JSON.parse(data);
+            resolve(object ? Object.keys(object) : []);
+          }
+        });
+      }
+      tryRequest();
+    });
+  };
+
+  /**
    * Turns Firebase low-level connection logging on or off.
    * @param {boolean | ROLLING} enable Whether to enable or disable logging, or enable rolling logs.
    *        Rolling logs can only be enabled once; any further calls to enableFirebaseLogging will
@@ -648,12 +676,6 @@ wrapNodeFire('orderByKey');
 wrapNodeFire('orderByValue');
 wrapNodeFire('orderByPriority');
 wrapNodeFire('ref');
-
-if (Firebase.prototype.childrenKeys) {
-  NodeFire.prototype.childrenKeys = function(options) {
-    return this.$firebase.childrenKeys(options);
-  };
-}
 
 
 // We need to wrap the user's callback so that we can wrap each snapshot, but must keep track of the
@@ -818,4 +840,3 @@ function invoke(op, options, fn) {
 }
 
 module.exports = NodeFire;
-
