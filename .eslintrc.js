81bda0df6a901221c91a0280eaf002de615d9acf
Piotr Kaminski <piotr@ideanest.com>
2018-10-10 19:52:18 -0700

    Update ESLint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   env: {
     node: true,
+    es6: true
   },
   parserOptions: {
     ecmaVersion: 2018
-  },
-  globals: {
-    Promise: false
   },
   extends: ['eslint:recommended'],
   rules: {
