722412940c56717131404ecc8f6cced26a1f5b52
Sebastian Noack <sebastian@reviewable.io>
2025-05-15 18:35:30 -0500

    Make update with empty object no-op (#45)

--- src/nodefire.ts
+++ src/nodefire.ts
@@ -311,6 +311,7 @@ export default class NodeFire {
    * or rejected with an error.
    */
   update(value: {[key: string]: Value}, options?: {timeout?: number}): Promise<void> {
+    if (_.isEmpty(value)) return Promise.resolve();
     return invoke(
       {ref: this, method: 'update', args: [value]}, options,
       (opts: any) => this.$ref.ref.update(value)
